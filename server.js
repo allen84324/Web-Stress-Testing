7ac0d0e70515c58c6e7d17dac814dece993b8421
allen84324 <allen84324@gmail.com>
2025-04-08 17:17:57 +0800

    修改後端

--- server.js
+++ server.js
@@ -75,5 +75,5 @@ app.post('/start-test', (req, res) => {
 		res.status(500).json({ error: '進程啟動失敗' })
 	})
 })
-
-app.listen(3000, () => console.log('Server running on port 3000'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
